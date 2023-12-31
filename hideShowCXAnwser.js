03f74d574fa766e3670fce74e9c224cfa076667d
LeonYew <learngenius@outlook.com>
2023-12-31 19:11:24 +0800

    update projectURL

--- hideShowCXAnwser.js
+++ hideShowCXAnwser.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         超星隐藏显示正确答案
 // @namespace    http://tampermonkey.net/
-// @version      v1.2.0
+// @version      v1.2.1
 // @description  通过单击键盘左上方的~/`/·键来隐藏/显示正确答案，用于期末复习
 // @author       LeonYew-SWPU
 // @match        https://mooc1.chaoxing.com/mooc-ans/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=chaoxing.com
+// @projectURL   https://github.com/LeonYew-SWPU/HideShowChaoXingAnswer
 // @license MIT
 // ==/UserScript==
 (function() {
